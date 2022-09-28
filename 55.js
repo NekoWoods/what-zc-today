ee004241e53ba0510d7da838a6b123e39fbedbcb
meowmeow <57374821+NekoWoods@users.noreply.github.com>
2022-09-29 01:02:39 +0800

    Update 55.js

--- 55.js
+++ 55.js
@@ -16,6 +16,12 @@ setInterval(myTimer, 1000);
 function myTimer() {
   const d = new Date();
   document.getElementById("time").innerHTML = d.toLocaleString();
+    
+  let add = (days(date_1, date_2) + 1) * 1000 * 60 * 90; //add 90 min to the start of this session, getting the end of this session
+  let diff = date_1.getTime() + add - d.getTime(); //calculate the diff between the end of this session and current time (in ms)
+  let min = Math.floor((diff / (1000 * 60))<<0);
+  let sec = Math.ceil((diff / 1000) % 60);
+  document.getElementById("timeLeft").innerHTML = "> 距离换图还有" + min + "分" + sec.toLocaleString("zh-CN",{minimumIntegerDigits: 2}) + "秒 <";
 }
 
 let remainder = days(date_1, date_2)%3;
@@ -37,4 +43,3 @@ function calculate(){
         document.getElementById("mainImage").src = img[2]};
 }
 calculate();
-
