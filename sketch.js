1d645ebcc9c2fe0c3715cac3f0706e162a992bf6
meowmeow <57374821+NekoWoods@users.noreply.github.com>
2023-09-19 20:06:11 +0800

    Update sketch.js
    阵地删掉惹

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 //https://www.freecodecamp.org/news/how-to-format-dates-in-javascript/
 
-let date_1 = new Date('09/19/2022 23:00:00'); //start date 09/19/2022 23:00:00 = zd
+let date_1 = new Date('09/18/2023 23:00:00'); //start date 09/18/2023 23:00:00 = cy
 let date_2 = new Date(); //today
 
 //days between today and the start date: days(date_1, date_2)
@@ -18,27 +18,26 @@ function myTimer() {
   document.getElementById("time").innerHTML = d.toLocaleString();
 }
 
-let remainder = days(date_1, date_2)%4;
+let remainder = days(date_1, date_2)%3;
 
 const img = [];
-img[0]='./assets/zd.png';
 img[1]='./assets/cf.png';
 img[2]='./assets/sb.png';
-img[3]='./assets/cy.png';
+img[0]='./assets/cy.png';
 
 function calculate(){
     if (remainder == 0){
-        document.title="今天什么战场？阵地", 
-        document.getElementById("mainImage").src = img[0]};
+        document.title="今天什么战场？草原", 
+        document.getElementById("mainImage").src = img[0],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是草原啦！"};
     if (remainder == 1){
         document.title="今天什么战场？尘封", 
-        document.getElementById("mainImage").src = img[1]};
+        document.getElementById("mainImage").src = img[1],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是尘封啦！"};
     if (remainder == 2){
         document.title="今天什么战场？碎冰", 
-        document.getElementById("mainImage").src = img[2]};
-    if (remainder == 3){
-        document.title="今天什么战场？草原", 
-        document.getElementById("mainImage").src = img[3]};
+        document.getElementById("mainImage").src = img[2],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是碎冰啦！"};
+
 }
 calculate();
-
