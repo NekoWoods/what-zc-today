1e9f6fac23095a0570def39bc27f66fde95ee975
meowmeow <57374821+NekoWoods@users.noreply.github.com>
2025-06-24 04:46:14 +0800

    timezone Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,53 @@
 //https://www.freecodecamp.org/news/how-to-format-dates-in-javascript/
 
-let date_1 = new Date('09/18/2023 23:00:00'); //start date 09/18/2023 23:00:00 = cy
+let date_1 = new Date('2025-06-23T15:00:00Z'); //start date UTC+8 2025/06/23 23:00:00 = sb
 let date_2 = new Date(); //today
 
 //days between today and the start date: days(date_1, date_2)
 const days = (date_1, date_2) =>{
-    let difference = date_2.getTime() - date_1.getTime();
+    let difference = (date_2.getTime() + date_2.getTimezoneOffset() * 1000 * 60) - 
+                    (date_1.getTime() + date_1.getTimezoneOffset() * 1000 * 60);//convert both dates to UTC in milliseconds and calculate the difference
     let TotalDays = Math.floor(difference / (1000 * 3600 * 24));
     return TotalDays;
 }
 
 
-//display time, refresh every sec
+//display UTC+8 time, refresh every sec
 setInterval(myTimer, 1000);
 function myTimer() {
   const d = new Date();
-  document.getElementById("time").innerHTML = d.toLocaleString();
+  document.getElementById("time").innerHTML = new Date(d.getTime() + d.getTimezoneOffset() * 1000 * 60 + 1000 * 3600 * 8).toLocaleString();
 }
 
-let remainder = days(date_1, date_2)%3;
+let remainder = days(date_1, date_2)%4;
 
 const img = [];
-img[1]='./assets/cf.png';
-img[2]='./assets/sb.png';
-img[0]='./assets/cy.png';
+img[3]='./assets/cf.png';
+img[0]='./assets/sb.png';
+img[1]='./assets/cy.png';
+img[2]='./assets/zd.png';
 
 function calculate(){
-    if (remainder == 0){
-        document.title="今天什么战场？草原", 
-        document.getElementById("mainImage").src = img[0],
-        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是草原啦！",
-        document.getElementById("zcrotation").innerHTML = "尘封>碎冰><b>草原</b>"};
     if (remainder == 1){
-        document.title="今天什么战场？尘封", 
+        document.title="今天什么战场？草原", 
         document.getElementById("mainImage").src = img[1],
-        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是尘封啦！",
-        document.getElementById("zcrotation").innerHTML = "<b>尘封</b>>碎冰>草原"};
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是草原啦！",
+        document.getElementById("zcrotation").innerHTML = "尘封>碎冰><b>草原</b>>阵地"};
     if (remainder == 2){
-        document.title="今天什么战场？碎冰", 
+        document.title="今天什么战场？阵地", 
         document.getElementById("mainImage").src = img[2],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是阵地啦！",
+        document.getElementById("zcrotation").innerHTML = "尘封>碎冰>草原><b>阵地</b>"};        
+    if (remainder == 3){
+        document.title="今天什么战场？尘封", 
+        document.getElementById("mainImage").src = img[3],
+        document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是尘封啦！",
+        document.getElementById("zcrotation").innerHTML = "<b>尘封</b>>碎冰>草原>阵地"};
+    if (remainder == 0){
+        document.title="今天什么战场？碎冰", 
+        document.getElementById("mainImage").src = img[0],
         document.getElementById("mainImage").alt = "你看到这行字因为图没显示出来 总之今天是碎冰啦！",
-        document.getElementById("zcrotation").innerHTML = "尘封><b>碎冰</b>>草原"};
+        document.getElementById("zcrotation").innerHTML = "尘封><b>碎冰</b>>草原>阵地"};
 
 }
 calculate();
